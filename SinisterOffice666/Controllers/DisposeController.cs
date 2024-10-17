9d6c632d564110b0235bbc3a635e314b784d2269
uuupppsss <ulya.ch.06@mail.ru>
2024-10-17 14:04:52 +1000

    Добавьте файлы проекта.

---
+++ SinisterOffice666/Controllers/DisposeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SinisterOffice666.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DisposeController : ControllerBase
+    {
+        
+    }
+}
